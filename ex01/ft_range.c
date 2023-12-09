1f8051e5a36a0fbd601b1a2737e5d2c71380f548
Anna Saenko <asaenko@c2r3s5.42luxembourg.lu>
2023-12-09 15:51:09 +0100

    01

--- ex01/ft_range.c
+++ ex01/ft_range.c
@@ -6,8 +6,45 @@
 /*   By: asaenko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 14:57:58 by asaenko           #+#    #+#             */
-/*   Updated: 2023/12/07 14:58:00 by asaenko          ###   ########.fr       */
+/*   Updated: 2023/12/09 15:49:40 by asaenko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdlib.h>
 
+int	*ft_range(int min, int max)
+{
+	int *arr;
+	int	i;
 
+	if (min <= max)
+	{
+		return(NULL);
+	}
+	arr = (int *)malloc(sizeof(int) * (max - min));
+	if (!arr)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (min < max)
+	{
+		arr[i] = min;
+		min++;
+		i++;
+	}
+	return (arr);
+}
+#include <stdio.h>
+int	main(void)
+{
+	int *arr = ft_range(3, 10);
+	int	i;
+
+	i = 0;
+	while(arr[i])
+	{
+		printf("%d\n", arr[i]);
+		i++;
+	}
+}
+	
