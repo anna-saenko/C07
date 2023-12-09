8fc7f8cbda2bdb14520e4bd38040b9786b90cfcf
Anna Saenko <asaenko@c2r3s5.42luxembourg.lu>
2023-12-09 15:19:17 +0100

    00

--- ex00/ft_strdup.c
+++ ex00/ft_strdup.c
@@ -6,8 +6,48 @@
 /*   By: asaenko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 14:57:26 by asaenko           #+#    #+#             */
-/*   Updated: 2023/12/07 14:57:29 by asaenko          ###   ########.fr       */
+/*   Updated: 2023/12/09 15:19:02 by asaenko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdlib.h>
 
+int	ft_strlen(char *str)
+{
+	int i;
 
+	i = 0;
+	while (int[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dup;
+
+	dup = malloc(ft_strlen(src) + 1);
+	dup = ft_strcpy(dup, src);
+	free(dup);
+	return (dup);
+}
+
+int	main(void)
+{
+
+}
