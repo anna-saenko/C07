60c545a9acc616f6f5b504223c93e891f19cccb1
Anna Saenko <asaenko@c2r3s5.42luxembourg.lu>
2023-12-09 16:13:00 +0100

    02

--- ex02/ft_ultimate.range.c
+++ ex02/ft_ultimate.range.c
@@ -6,8 +6,31 @@
 /*   By: asaenko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 14:59:15 by asaenko           #+#    #+#             */
-/*   Updated: 2023/12/07 14:59:17 by asaenko          ###   ########.fr       */
+/*   Updated: 2023/12/09 16:12:40 by asaenko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+
+	if (min <= max)
+	{
+		return (0);
+	}	
+	range = (int *)malloc(sizeof(int) * (max - min));
+	if (!range)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (min < max)
+	{
+		range[i] = min;
+		min++;
+		i++;
+	}
+	return (i);
+}
