7619dc8575b2857e2b4e3ab8842943f80792fd3d
Anna Saenko <asaenko@c2r3s5.42luxembourg.lu>
2023-12-11 14:44:20 +0100

    fixed 01

--- ex01/ft_range.c
+++ ex01/ft_range.c
@@ -6,45 +6,51 @@
 /*   By: asaenko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 14:57:58 by asaenko           #+#    #+#             */
-/*   Updated: 2023/12/09 15:49:40 by asaenko          ###   ########.fr       */
+/*   Updated: 2023/12/11 14:43:55 by asaenko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
 
 int	*ft_range(int min, int max)
 {
-	int *arr;
+	int	*arr;
 	int	i;
 
-	if (min <= max)
+	if (min >= max)
 	{
-		return(NULL);
+		return (0);
 	}
-	arr = (int *)malloc(sizeof(int) * (max - min));
-	if (!arr)
+	else
 	{
-		return (NULL);
+		arr = (int *)malloc(sizeof(int) * (max - min));
+		i = 0;
+		while (min < max)
+		{
+			arr[i] = min;
+			min++;
+			i++;
+		}
+		return (arr);
 	}
-	i = 0;
-	while (min < max)
-	{
-		arr[i] = min;
-		min++;
-		i++;
-	}
-	return (arr);
 }
+/*
 #include <stdio.h>
 int	main(void)
 {
-	int *arr = ft_range(3, 10);
+	int *arr = ft_range(10, 30);
 	int	i;
 
 	i = 0;
-	while(arr[i])
+	if (arr)
 	{
-		printf("%d\n", arr[i]);
-		i++;
+		while(arr[i])
+		{
+			printf("%d\n", arr[i]);
+			i++;
+		}
 	}
-}
-	
+	else
+	{
+		printf("The min has to be less than max");
+	}
+}*/
